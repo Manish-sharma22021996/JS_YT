a32101057245bca3d750f582d4859a6ea0dce728
Manish Sharma <117812620+Manish-sharma22021996@users.noreply.github.com>
2024-05-27 17:07:11 +0000

    studied abt variables and datatype

---
+++ 01_Basic/datatype.js
@@ -0,0 +1,11 @@
+let name = "Manish"; // String
+let num = 23  // Number
+let flag = false; // boolean
+
+/*
+    null => Standalone value
+    undefined => value yet to be assigned
+*/
+
+console.log(typeof null); // will be  Object[bug in JS]
+console.log(typeof undefined); // will be undefined
---
+++ 01_Basic/variables.js
@@ -0,0 +1,14 @@
+const accId = 1232;
+let name = "Manish"
+
+// try to avoid using below way of defing variables
+var add = "AGRA";
+pincode = 232343
+
+// below will give us undefined
+let accountState;
+
+
+
+
+console.table([accId, name, add, pincode, accountState]);
