ef7fe2c8608d9d22b11e4ad723af71e94f128329
Manish Sharma <117812620+Manish-sharma22021996@users.noreply.github.com>
2024-05-27 19:46:07 +0000

    some changes done

---
+++ 01_Basic/conversionTypes.js
@@ -0,0 +1,16 @@
+let score = "33"
+
+let num = "22qw"
+
+let num1 = null
+let num2 = undefined
+
+let strscore = Number(score);  // 33
+let strscore1 = Number(num);//  NaN
+let strscore2 = Number(num1); // 0
+let strscore3 = Number(num2);// NaN
+
+console.log( strscore)
+console.log( strscore1)
+console.log( strscore2)
+console.log( strscore3)
