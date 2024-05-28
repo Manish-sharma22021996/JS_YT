1c3330ed303595d26efd45a1320e7c4c47323ac3
Manish Sharma <117812620+Manish-sharma22021996@users.noreply.github.com>
2024-05-28 16:40:11 +0000

    String and Number& Math file added.

---
+++ 01_Basic/num.js
@@ -0,0 +1,25 @@
+const score = 300;
+const balance = new Number(400000);
+
+// console.log(score)
+// console.log(score.toString())
+// console.log(balance.toFixed(2))
+// console.log(balance.toLocaleString())
+// console.log(balance.toLocaleString('en-IN'))
+// console.log(balance)
+
+// const otherNum = 123.342131
+// console.log(balance.toPrecision(3))
+
+
+// ++++++++++  Math +++++++
+
+// console.log(Math)
+// console.log(Math.round(4.3))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.3))
+// console.log(Math.abs(-4.3))
+// console.log(Math.min(4,5,2,7,9))
+// console.log(Math.max(4,5,2,7,9))
+
+console.log(Math.floor(Math.random()* 10) + 1)
---
+++ 01_Basic/string.js
@@ -0,0 +1,46 @@
+const str = "Manish";
+
+let strLen = str.length;
+
+// console.log(str.toUpperCase());
+// console.log(str.charAt(3));
+// console.log(str.startsWith('a'));
+// console.log(str.slice(2));
+// console.log(str.substring(2,5));
+// console.log(str.substr(2));
+
+// console.log(str.startsWith('Ma'));
+
+// console.log(str.includes('nis'))
+
+// const strNum = '34'
+// const numStr = '345as'
+// const numSttr = 'as45'
+
+// console.log(parseInt(strNum));
+// console.log(parseInt(numStr));
+// console.log(Number(numStr));
+// console.log(parseInt(numSttr));
+
+//  similerly we have parseFloat method
+
+
+// console.log(str.replace('sh', 'shSakshi'));
+
+
+// string to arr
+
+// const arr = str.split("");
+// console.log(arr);
+
+
+// const name = "Sakshi";
+// const add = "Agra"
+
+
+// console.log(`${name} lives in my Heart and we lives in ${add}`)
+
+// const strObj = new String('hello world! ')
+// console.log(strObj.endsWith(' '))
+
+
