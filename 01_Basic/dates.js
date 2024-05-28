e5750e7296b6b56d9990677a0137c4e4564e2101
Manish Sharma <117812620+Manish-sharma22021996@users.noreply.github.com>
2024-05-28 17:35:00 +0000

    date and array updates

---
+++ 01_Basic/dates.js
@@ -0,0 +1,16 @@
+// Dates
+
+// const myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// const createDate = new Date("01-04-2024");
+// console.log(createDate)
+
+
+let timeStamp = Date.now();
+
+console.log(timeStamp)
+console.log(`Time in seconds ${Math.floor(Date.now()/1000)}`)
