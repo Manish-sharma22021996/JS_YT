d64a49285795c93883905b57414fb1b7f4f5cf61
Manish Sharma <117812620+Manish-sharma22021996@users.noreply.github.com>
2024-05-28 18:11:15 +0000

    array updated

---
+++ 02_Basic/array.js
@@ -0,0 +1,24 @@
+//  arrays
+
+const myarr = [1, 3, 5, 4, 6, 6]
+
+const myarr1 = [2,3,4,5]
+
+// console.log(myarr.concat(myarr1));
+
+let merge = [...myarr, ...myarr1]
+
+// console.log(merge)
+
+
+
+
+// splice and slice
+console.log(myarr.slice(2))
+console.log(myarr)
+
+console.log(myarr.splice(2))
+console.log(myarr)
+
+
+
